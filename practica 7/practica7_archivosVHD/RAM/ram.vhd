562b4e4f10fa45b9c8d627c65c1c3575e44fd832
ManuGS2 <manu.skate17@gmail.com>
2019-10-23 21:47:25 -0500

    Programa cargado en memoria

--- practica 7/practica7_archivosVHD/RAM/ram.vhd
+++ practica 7/practica7_archivosVHD/RAM/ram.vhd
@@ -22,14 +22,42 @@ architecture Behavioral of ram is
 	-- Memory Write Block
 	MEM_WRITE: process (address, CSn, Wn, data) 
 	begin
-		mem(0) <= X"C6";
-		mem(1) <= X"02";
-		mem(2) <= X"86";
-		mem(3) <= X"00";
-		mem(4) <= X"1B";
-		mem(5) <= X"7E";
-		mem(6) <= X"00";
-		mem(7) <= X"04";
+		mem(0) <= x"86"; -- LDAA #$00 Cargamos en A 00 por acceso inmediato
+		mem(1) <= x"00";
+		mem(2) <= x"97"; -- STAA #0050 Cargamos en la dir 50 el contenido de A 'suma'
+		mem(3) <= x"50";
+		mem(4) <= x"86"; -- LDAA #$01 Cargamos en A 01 que será 'i'
+		mem(5) <= x"01";
+		mem(6) <= x"97"; -- STAA #0051 Cargamos en la dir 51 el contenido de A
+		mem(7) <= x"51";
+		mem(8) <= x"D6"; -- LDAB #0051 Cargamos el valor de i en B
+		mem(9) <= x"51";
+		mem(10) <= x"86"; -- LDAA #$10 Cargamos 10 (0A) en A 'n'
+		mem(11) <= x"0A";
+		mem(12) <= x"11"; -- CBA Comparas A con B (A-B)
+		mem(13) <= x"27"; -- JE Si el resultado es igual a 0
+		mem(14) <= x"1E"; -- x"1E" = 30 decimal
+		mem(15) <= x"96"; -- LDAA #$0050 
+		mem(16) <= x"50";
+		mem(17) <= x"1B"; -- ABA A <- A+B
+		mem(18) <= x"5C"; -- INCB B <- B+1
+		mem(19) <= x"97"; -- STAA #0050
+		mem(20) <= x"50";
+		mem(21) <= x"D7"; -- STAB #0051
+		mem(22) <= x"51";
+		mem(23) <= x"7E"; -- JMP al inicio
+		mem(24) <= x"00";
+		mem(25) <= x"0A";
+		
+		mem(30) <= X"C6";
+		mem(31) <= X"02";
+		mem(32) <= X"86";
+		mem(33) <= X"00";
+		mem(34) <= X"1B";
+		mem(35) <= X"7E";
+		mem(36) <= X"00";
+		mem(37) <= X"04";
+		
 		
 		if (CSn = '0' and Wn = '0') then
 			mem(conv_integer(unsigned(address))) <= data;
